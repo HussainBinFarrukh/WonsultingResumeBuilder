733c2895cf215dab231d9d5e3bc165675b5eb6b7
hussain <hussain.13@live.com>
2020-10-12 16:48:59 -0400

    Updates

---
+++ precache-manifest.b9ca2bf47c2ac956df28256996e60397.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f5a6b38c5626fae26d579a89343acd",
+    "url": "/WonsultingResumeBuilder/index.html"
+  },
+  {
+    "revision": "64057c6eb13ad72ab836",
+    "url": "/WonsultingResumeBuilder/static/css/main.d273553d.chunk.css"
+  },
+  {
+    "revision": "5906c826a92a93d93b36",
+    "url": "/WonsultingResumeBuilder/static/js/2.538e842d.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/WonsultingResumeBuilder/static/js/2.538e842d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64057c6eb13ad72ab836",
+    "url": "/WonsultingResumeBuilder/static/js/main.d6ae4806.chunk.js"
+  },
+  {
+    "revision": "5c21b6840243737fc56b",
+    "url": "/WonsultingResumeBuilder/static/js/runtime-main.a305818a.js"
+  },
+  {
+    "revision": "0b4a9a0de55415b8532f5f3fb88563d4",
+    "url": "/WonsultingResumeBuilder/static/media/Gradient with White Text-4500x4500.0b4a9a0d.png"
+  },
+  {
+    "revision": "07b749f3d0ddc539382effe4a40a4359",
+    "url": "/WonsultingResumeBuilder/static/media/default.07b749f3.png"
+  },
+  {
+    "revision": "e7c51d50b4f49144ac120faae7c4027c",
+    "url": "/WonsultingResumeBuilder/static/media/templateA.e7c51d50.png"
+  }
+]);
